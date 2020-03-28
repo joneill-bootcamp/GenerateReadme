e41c865e6f94eb92a3a18a797e045d83b9d2cc6b
John O'Neil <fluffywestie@tpg.com.au>
2020-03-28 19:35:35 +1100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const questions = [{
+        type: "input",
+        message: "What is your user name?",
+        name: "username"
+    },
+    {
+        type: "password",
+        message: "What is your password?",
+        name: "password"
+    },
+    {
+        type: "password",
+        message: "Re-enter password to confirm:",
+        name: "confirm"
+    }
+
+];
+
+function writeToFile(fileName, data) {}
+
+function init() {
+
+
+}
+
+init();
