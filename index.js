9cd76bd40c5c71275ad3f868974bf65846683550
John O'Neil <fluffywestie@tpg.com.au>
2020-03-28 20:00:09 +1100

    Require statements included, defined questions, write data to file implemented

--- index.js
+++ index.js
@@ -1,26 +1,63 @@
+// Bring in required libraries 
+const inquirer = require("inquirer");
+const generateMarkDown = require("./utils/generateMarkdown.js");
+const api = require("./utils/api.js");
+const fs = require("fs");
+
+/* title, description, installation, and usage sections */
+
+
+// Define constants
 const questions = [{
         type: "input",
-        message: "What is your user name?",
-        name: "username"
+        message: "Please provide your GitHub username: ",
+        name: "githubuser"
     },
     {
-        type: "password",
-        message: "What is your password?",
-        name: "password"
+        type: "input",
+        message: "Please provide a title for the project: ",
+        name: "title"
     },
     {
-        type: "password",
-        message: "Re-enter password to confirm:",
-        name: "confirm"
+        type: "input",
+        message: "Please provide a short description of project: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "How do we install the project once cloned? ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "How do we run/use this application? ",
+        name: "usage"
     }
-
 ];
 
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile("log.txt", process.argv[2], function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+
+    });
+
+
+}
 
 function init() {
 
-
+    inquirer
+        .prompt(questions)
+        .then(function (response) {
+            // process responses
+            console.log(response);
+        });
 }
 
 init();
