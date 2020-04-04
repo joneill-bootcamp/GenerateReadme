718052316ed8e1913831db101b201ce80ab2ab5e
John O'Neil <fluffywestie@tpg.com.au>
2020-04-04 19:46:30 +1100

    Updated file logic

--- README.md
+++ README.md
@@ -1,31 +1,15 @@
-# README Generator
 
-## Table of Contents
+# dkjsdlfjhsdfa
 
-- [Installing](#Installing)
-- [Instructions](#Instructions)
-- [Technology Used](#Technology)
-- [Team](#Team)
+# sdfklsadflkasdf
 
-## Installing
+## dfklsadjfasd
 
-npm install
+## fasldkfjlskdjfasd
 
-## Instructions
+## fsakdjfslkdjfsa
 
-Simply enter yout GitHub username and Repository that you wish to generate a README.md for, then answer the prompts and a basic README.md file will be created.
-If you have an Avatar on your gitHub account and an email address saved, these will be included
+## sdioruwerwe
 
-<img width="734" alt="image" src="https://user-images.githubusercontent.com/59762660/77823280-098f5e80-714e-11ea-9ebc-b311d4d44df8.png">
+## lkdfslkdfsdf
 
-That's it, a simple command line tool.
-
-## Technology
-
-- Axios
-- Node.js
-
-## Team
-
-John O'Neill
-(and of course.. Pippy!)
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Bring in required libraries 
 const inquirer = require("inquirer");
 const generateMarkDown = require("./utils/generateMarkdown.js");
-const generateMarkdown1 = require("./utils/generateMarkdown.js");
+const generateMarkDown1 = require("./utils/generateMarkdown.js");
 const fs = require("fs");
 const axios = require("axios");
 
 // Define Questions
 const questions = [{
+        type: "input",
+        message: "Please state name of the file you want to write this content to (newREAMD.md if left blank): ",
+        name: "filename"
+    }, {
         type: "input",
         message: "Please provide your GitHub username: ",
         name: "gitHubUser"
@@ -79,7 +83,7 @@ function appendToFile(fileName, data) {
         if (err) {
             return console.log(err);
         }
-        console.log("Success!");
+        console.log("Success!, new README.md file content written to 'newREADME.md', please feel free to use.");
     });
 }
 
@@ -89,22 +93,33 @@ function init() {
         .prompt(questions)
         .then(function (response) {
 
+            if (!response.fileName) {
+                let filename = "newREADME.md"
+            } else {
+                let filename = response.fileName;
+            }
             // Write responses to quetions
-            writeToFile("README.md", generateMarkDown(response));
+            writeToFile(filename, generateMarkDown(response));
 
-            // Call GitHub API to obtain details on repo owner email
-            axios.get('https://api.github.com/repos/' + response.gitHubUser + '/' + response.repo)
-                .then((response1) => {
-                    let avatar_url = response1.data.owner.avatar_url;
-                    appendToFile("README.md", avatar_url);
-                });
-
-            // call GitHub API to obtain users email
-            axios.get('https://api.github.com/users/' + response.gitHubUser)
-                .then((response1) => {
-                    let email = response1.data.email
-                    appendToFile("README.md", email);
-                });
+            try { // Call GitHub API to obtain details on repo owner email
+                axios.get('https://api.github.com/repos/' + response.gitHubUser + '/' + response.repo)
+                    .then((response1) => {
+                        let avatar_url = response1.data.owner.avatar_url;
+                        appendToFile(filename, avatar_url);
+                    });
+            } catch (error) {
+                console.log(error);
+            }
+            // try {
+            //     // call GitHub API to obtain users email
+            //     axios.get('https://api.github.com/users/' + response.gitHubUser)
+            //         .then((response1) => {
+            //             let email = response1.data.email
+            //             appendToFile("newREADME.md", email);
+            //         });
+            // } catch (error) {
+            //     console.log(error);
+            // }
         });
 }
 
